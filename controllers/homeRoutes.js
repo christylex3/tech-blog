093eae57b3d788ea1b2e73b1b5913010058aed1e
Christy Le <christylex3@gmail.com>
2022-08-12 21:52:29 -0400

    Installed bcrypt. Adding GET all blogs route in controller/homeRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const { Blog, User } = require("../models");
+// const loginAuth = require("../utils");
+
+
+// GET all blogs and the name of users
+router.get("/", async (req, res) => {
+    try {
+        // Finds all blogs along with the name of the users
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["name"],
+                }
+            ],
+        })
+
+        // Serializes data so the template can read it
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        // Renders the serialized data into a template
+        res.render("homepage", {
+            blogs,
+            // logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
