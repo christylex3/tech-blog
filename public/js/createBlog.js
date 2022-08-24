572001047885fa960f7772a599dd430080919a6b
Christy Le <christylex3@gmail.com>
2022-08-24 10:47:53 -0400

    Enable created blogs to show up on homepage and on dashboard. Fixing user's email to show up... Need to fix timestamp

--- public/js/createBlog.js
+++ public/js/createBlog.js
@@ -1,10 +1,30 @@
-function createBlogHandler(event) {
+async function createBlogHandler(event) {
+    console.log("Am I showing up here?");
     
-    document.location.replace("/api/createBlog");
+    // document.location.replace("/api/createBlog");
     event.preventDefault();
+    console.log("Am I showing up here too?");
 
-    var blogName = document.querySelector("#blog-name").value.trim();
-    var blogBody = document.querySelector("#blog-body").value.trim();
+    const blogName = document.querySelector("#blog-name").value.trim();
+    const blogBody = document.querySelector("#blog-body").value.trim();
 
+    if (blogName && blogBody) {
+        const response = await fetch(`/api/blogs`, {
+            method: `POST`,
+            // body: JSON.stringify({ name: blogName, body: blogBody }),
+            body: JSON.stringify({ blogName, blogBody }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
 
-}
+        console.log(response);
+        if (response.ok) {
+            console.log("CREATED A BLOG!!!!***********");
+            document.location.replace("/dashboard");
+            
+        } else {
+            console.log("Failed to create a blog");
+        }
+    }
+};
